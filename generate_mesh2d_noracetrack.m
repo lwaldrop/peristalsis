8ba95bf133031cd782de3157d8e6e9137635dc6d
Lindsay Waldrop <lwaldrop@email.unc.edu>
2015-06-13 15:21:17 -0400

    Small changes to no racetrack again

--- generate_mesh2d_noracetrack.m
+++ generate_mesh2d_noracetrack.m
@@ -242,27 +242,27 @@ fclose(beam_fid);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Write out the target point information for the ends of the elastic tube
-target_fid = fopen([mesh_name 'tube_' num2str(N) '.target'], 'w');
+%target_fid = fopen([mesh_name 'tube_' num2str(N) '.target'], 'w');
 
-fprintf(target_fid, '%d\n', 4*Nend);
+%fprintf(target_fid, '%d\n', 4*Nend);
 
-for i = 0:Nend-1,
-    fprintf(target_fid, '%d %1.16e\n', i, kappa_target*ds/(ds^2));
-end
+%for i = 0:Nend-1,
+%    fprintf(target_fid, '%d %1.16e\n', i, kappa_target*ds/(ds^2));
+%end
 
-for i = ceil(Nstraight/2)-Nend:ceil(Nstraight/2)-1,
-    fprintf(target_fid, '%d %1.16e\n', i, kappa_target*ds/(ds^2));
-end
+%for i = 0ceil(Nstraight/2):ceil(Nstraight/2)-1,
+%    fprintf(target_fid, '%d %1.16e\n', i, kappa_target*ds/(ds^2));
+%end
 
-for i = ceil(Nstraight/2):ceil(Nstraight/2)+Nend-1,
-    fprintf(target_fid, '%d %1.16e\n', i, kappa_target*ds/(ds^2));
-end
+%for i = ceil(Nstraight/2):ceil(Nstraight/2)+Nend-1,
+%    fprintf(target_fid, '%d %1.16e\n', i, kappa_target*ds/(ds^2));
+%end
 
-for i = Nstraight-Nend:Nstraight-1,
-    fprintf(target_fid, '%d %1.16e\n', i, kappa_target*ds/(ds^2));
-end
+%for i = Nstraight-Nend:Nstraight-1,
+%    fprintf(target_fid, '%d %1.16e\n', i, kappa_target*ds/(ds^2));
+%end
 
-fclose(target_fid);
+%fclose(target_fid);
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
